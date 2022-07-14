c437b06df1e8b61bd7f369744be8bc7f8ea591be
Skaletto07 <Skaletto07@yandex.ru>
2022-07-14 12:49:21 +0300

    singleton1'
    cx
    
    asdf
    ;

--- singleton/Main.java
+++ singleton/Main.java
@@ -5,6 +5,15 @@ public class Main {
         Singleton singleton = Singleton.getInstance();
         System.out.println(singleton);
 
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println(singleton1);
+
+        Water water = Water.getInstance();
+        System.out.println(water);
+
+        Water water1 = Water.getInstance();
+        System.out.println(water1);
+
 
 
     }
--- singleton/Water.java
+++ singleton/Water.java
@@ -1,2 +1,20 @@
-package patterns.singleton;public class Water {
+package patterns.singleton;
+
+public class Water {
+    private static volatile Water instance;
+
+    private Water() {}
+
+    public static Water getInstance() {
+       Water localInstance = instance;
+       if (localInstance == null) {
+           synchronized (Water.class) {
+               localInstance = instance;
+               if (localInstance == null) {
+                   instance = localInstance = new Water();
+               }
+           }
+       }
+       return localInstance;
+    }
 }
