2f060c612ddbc7c53187928c54c4b75aba96a66d
Skaletto07 <Skaletto07@yandex.ru>
2022-07-14 13:05:01 +0300

    Multiton

--- muliton/Main.java
+++ muliton/Main.java
@@ -1,2 +1,14 @@
-package patterns.muliton;public class Main {
+package patterns.muliton;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Multiton multiton = Multiton.getInstance(Multiton.Count.ZERO);
+        System.out.println(multiton);
+
+        Multiton multiton2 = Multiton.getInstance(Multiton.Count.TWO);
+        System.out.println(multiton2);
+
+
+    }
 }
--- muliton/Multiton.java
+++ muliton/Multiton.java
@@ -1,2 +1,30 @@
-package patterns.muliton;public class Multiton {
+package patterns.muliton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Multiton {
+    public static enum Count{
+        ZERO,
+        ONE,
+        TWO,
+        THREE
+    }
+
+    private Multiton() {
+    }
+
+    private static Map<Count, Multiton> multitonMap = new HashMap();
+
+    private static Multiton instance;
+
+    public static Multiton getInstance(Count key) {
+        instance = multitonMap.get(key);
+        if (instance == null) {
+            instance = new Multiton();
+            multitonMap.put(key, instance);
+        }
+        return instance;
+    }
+
 }
